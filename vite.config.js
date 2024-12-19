2d4e56a4598894afad21997c52195da0bc59a5fb
mfthlhdz <145913649+mfthlhdz@users.noreply.github.com>
2024-12-19 23:20:35 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +7,11 @@ export default defineConfig({
   server: {
     port: 3726,
   },
-})
+  build: {
+    outDir: 'dist',  // Tentukan folder output untuk build
+    assetsDir: 'assets',  // Tentukan folder untuk menyimpan file aset statis (gambar, font, dll.)
+    rollupOptions: {
+      input: '/src/index.js', // Tentukan file utama yang digunakan di aplikasi Anda (jika berbeda)
+    },
+  },
+});
