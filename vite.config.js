62bd204afcfbbc64790e08b172b39cafd87b9fc4
mfthlhdz <145913649+mfthlhdz@users.noreply.github.com>
2024-12-20 01:13:54 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePluginCopy } from 'vite-plugin-copy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginCopy({
-      targets: [
-        { src: 'src/assets/*', dest: 'dist/assets' },
-        { src: 'src/css/*', dest: 'dist/css' },
-        { src: 'src/fonts/*', dest: 'dist/fonts' },
-        { src: 'src/components/*', dest: 'dist/component' },
-        { src: 'src/pages/*', dest: 'dist/pages' },
-      ],
-      hook: 'buildEnd',  // Mengatur hook setelah build selesai
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 3726,
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-});
+})
