a52163207f294057abd74210fc19779130914ef8
mfthlhdz <145913649+mfthlhdz@users.noreply.github.com>
2024-12-19 23:33:32 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import VitePluginStaticCopy from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/*',  // Sesuaikan dengan lokasi file yang dibutuhkan
+          dest: 'assets',  // Menyalin ke dalam folder dist/assets
+        },
+        {
+          src: 'src/css/*',
+          dest: 'css',  // Menyalin CSS ke dalam folder dist/css
+        },
+        {
+          src: 'src/font/*',
+          dest: 'font',  // Menyalin font ke dalam folder dist/font
+        },
+      ],
+    }),
+  ],
   server: {
     port: 3726,
   },
   build: {
-    outDir: 'dist',  // Tentukan folder output untuk build
-    assetsDir: 'assets',  // Tentukan folder untuk menyimpan file aset statis (gambar, font, dll.)
-    rollupOptions: {
-      input: 'index.html', // Tentukan file utama yang digunakan di aplikasi Anda (jika berbeda)
-    },
+    outDir: 'dist', // Output build ke folder dist
+    emptyOutDir: true, // Menghapus konten lama di dist/
   },
 });
