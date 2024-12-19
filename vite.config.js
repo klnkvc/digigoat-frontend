eab8f7eb343272c53d562f21b5d2de7aba2af5d7
mfthlhdz <145913649+mfthlhdz@users.noreply.github.com>
2024-12-20 00:52:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
+import VitePluginCopy from 'vite-plugin-copy';
 
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'copy-assets',
-      buildStart() {
-        const srcDir = path.resolve(__dirname, 'src');
-        const distDir = path.resolve(__dirname, 'dist');
-
-        // Daftar file dan folder yang ingin disalin
-        const directoriesToCopy = ['assets', 'css', 'font', 'components', 'pages'];
-
-        directoriesToCopy.forEach(dir => {
-          const sourceDir = path.join(srcDir, dir);
-          const destDir = path.join(distDir, dir);
-
-          if (fs.existsSync(sourceDir)) {
-            // Membuat folder tujuan jika belum ada
-            fs.mkdirSync(destDir, { recursive: true });
-
-            // Menyalin file dari src ke dist
-            fs.readdirSync(sourceDir).forEach(file => {
-              const srcFile = path.join(sourceDir, file);
-              const destFile = path.join(destDir, file);
-              fs.copyFileSync(srcFile, destFile);
-            });
-          }
-        });
-      },
-    },
+    VitePluginCopy({
+      targets: [
+        { src: 'src/assets/*', dest: 'dist/assets' },
+        { src: 'src/css/*', dest: 'dist/css' },
+        { src: 'src/fonts/*', dest: 'dist/fonts' },
+        { src: 'src/components/*', dest: 'dist/component' },
+        { src: 'src/pages/*', dest: 'dist/pages' },
+      ],
+      hook: 'buildEnd',  // Mengatur hook setelah build selesai
+    })
   ],
   server: {
     port: 3726,
