b50a7086c3007bae5ca9400c840895f0ff49f624
mfthlhdz <145913649+mfthlhdz@users.noreply.github.com>
2024-12-20 00:35:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3726,
   },
+  build: {
+    outDir: 'dist', // Pastikan build menghasilkan ke folder dist
+  }
 })
